aaa913632c2a7af9ac1903b244ad9b3f2641f5a5
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-03-10 01:34:09 +0100

    Update socket_functions.py

--- network/socket_functions.py
+++ network/socket_functions.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 
 class SocketError(Exception):
   """Exception raised for errors relating to the socket."""
@@ -7,7 +8,7 @@ class SocketError(Exception):
 
 class SocketPS:
   SUCCESS = '{"ResponseType": "srOk"}\r\n'
-  def __init__(self, HOST: str, PORT: int, maxConnections = 16) -> None:
+  def __init__(self, HOST: str, PORT: int, maxConnections: int = 16) -> None:
     self.HOST = HOST
     self.PORT = PORT
     self.semaphore = asyncio.Semaphore(maxConnections) # Maximum 16 mounts at once
@@ -17,12 +18,12 @@ class SocketPS:
     try:
       async with self.semaphore:
         reader, writer = await asyncio.open_connection(host, port)
-        writer.write(message.encode())
+        writer.write(message.encode("utf-8"))
         await writer.drain()
       
         response = await reader.read(1024)
 
-        return response.decode()
+        return response.decode("utf-8")
     
     except (ConnectionError, asyncio.TimeoutError) as e:
       print(f"An error occured while sending tcp message: {e}")
@@ -38,19 +39,15 @@ class SocketPS:
     writer.close()
     await writer.wait_closed()
 
-  #async def socket_list(self, random_string: str, savename: str) -> None:
-  #  message = f"""{{"RequestType": "rtListSaveFiles", "listTargetSaveName": "{savename}"}}\r\n"""
-  #  response = await self.send_tcp_message_with_response(self.HOST, self.PORT, message)
-
   async def socket_dump(self, random_string: str, savename: str) -> None: 
-    message = f'''{{"RequestType": "rtDumpSave", "sourceSaveName": "{savename}", "targetFolder": "{random_string}", "dumpOnly": []}}\r\n'''
-    response = await self.send_tcp_message_with_response(self.HOST, self.PORT, message)
-
+    request = f'{{"RequestType": "rtDumpSave", "dump": {{"saveName": "{savename}", "targetFolder": "{random_string}", "selectOnly": []}}}}\r\n'
+    response = await self.send_tcp_message_with_response(self.HOST, self.PORT, request)
+    
     if response != self.SUCCESS:
       raise SocketError("Invalid save!")
 
   async def socket_update(self, random_string: str, savename: str) -> None: 
-    message = f"""{{"RequestType": "rtUpdateSave", "targetSaveName": "{savename}", "sourceFolder": "{random_string}", "selectOnly": []}}\r\n"""
+    message = f'{{"RequestType": "rtUpdateSave", "update": {{"saveName": "{savename}", "sourceFolder": "{random_string}", "selectOnly": []}}}}\r\n'
     response = await self.send_tcp_message_with_response(self.HOST, self.PORT, message)
 
     if response != self.SUCCESS:
