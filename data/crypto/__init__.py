22d3a1a4af9c2df6555f4d804bebdd7498bec480
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-03-10 01:31:56 +0100

    Update __init__.py

--- data/crypto/__init__.py
+++ data/crypto/__init__.py
@@ -2,3 +2,6 @@ from .common import CryptoError, CustomCrypto
 from .rstar_crypt import Crypt_Rstar
 from .xeno2_crypt import Crypt_Xeno2
 from .bl3_crypt import Crypt_BL3
+from .ndog_crypt import Crypt_Ndog
+from .mgsv_crypt import Crypt_MGSV
+from .rev2_crypt import Crypt_Rev2
