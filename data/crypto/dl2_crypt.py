c82f9a90bc01ab80e12f339a1e86ce581405c720
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-03-16 16:13:09 +0100

    Add files via upload

---
+++ data/crypto/dl2_crypt.py
@@ -0,0 +1,38 @@
+import aiofiles
+import gzip
+import os
+from .common import CustomCrypto
+
+class Crypt_DL2:
+    @staticmethod
+    async def decryptFile(folderPath: str) -> None:
+        files = CustomCrypto.obtainFiles(folderPath)
+
+        for fileName in files:
+            filePath = os.path.join(folderPath, fileName)
+
+            async with aiofiles.open(filePath, "rb") as savegame:
+                compressed_data = await savegame.read()
+
+            uncompressed_data = gzip.decompress(compressed_data)
+
+            async with aiofiles.open(filePath, "wb") as savegame:
+                await savegame.write(uncompressed_data)
+    
+    @staticmethod
+    async def encryptFile(fileToEncrypt: str) -> None:
+        async with aiofiles.open(fileToEncrypt, "rb") as savegame:
+            uncompressed_data = await savegame.read()
+
+        compressed_data = gzip.compress(uncompressed_data)
+
+        async with aiofiles.open(fileToEncrypt, "wb") as savegame:
+            await savegame.write(compressed_data)
+
+    @staticmethod
+    async def checkEnc_ps(fileName: str) -> None:
+        async with aiofiles.open(fileName, "rb") as savegame:
+            magic = await savegame.read(3)
+        
+        if magic == b"\x1F\x8B\x08":
+            await Crypt_DL2.encryptFile(fileName)
