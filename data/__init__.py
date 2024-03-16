eccad47233d44afbe048373c335e1484fc1ddb8d
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-03-16 16:13:29 +0100

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -12,3 +12,4 @@ from data.crypto.xeno2_crypt import Crypt_Xeno2
 from data.crypto.ndog_crypt import Crypt_Ndog
 from data.crypto.mgsv_crypt import Crypt_MGSV
 from data.crypto.rev2_crypt import Crypt_Rev2
+from data.crypto.dl2_crypt import Crypt_DL2
