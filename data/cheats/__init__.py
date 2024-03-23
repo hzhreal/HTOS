898219d69d80a243b7dd3281936a1b76c27f3be0
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-03-23 22:04:33 +0100

    Update __init__.py

--- data/cheats/__init__.py
+++ data/cheats/__init__.py
@@ -1,3 +1,4 @@
 from .common import QuickCheatsError, QuickCheats, TimeoutHelper
+from .quickcodes import QuickCodes, QuickCodesError
 from .gtav_cheats import Cheats_GTAV
 from .rdr2_cheats import Cheats_RDR2
