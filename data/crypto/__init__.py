d92cf41613cc5d6b8103eab43ef968f7fe58dc64
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-03-31 09:28:51 +0200

    Update __init__.py

--- data/crypto/__init__.py
+++ data/crypto/__init__.py
@@ -5,6 +5,6 @@ from .bl3_crypt import Crypt_BL3
 from .ndog_crypt import Crypt_Ndog
 from .mgsv_crypt import Crypt_MGSV
 from .rev2_crypt import Crypt_Rev2
-from .dl2_crypt import Crypt_DL2
+from .dl_crypt import Crypt_DL
 from .rgg_crypt import Crypt_RGG
 from .di2_crypt import Crypt_DI2
