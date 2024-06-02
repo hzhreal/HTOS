66ccd8a2adcf9d1c2653c5cf2fed61c9ac5967a9
hzhreal <kerimc247@gmail.com>
2024-06-03 00:22:27 +0200

    switch to cecie command

--- cogs/sealed_key.py
+++ cogs/sealed_key.py
@@ -1,33 +1,11 @@
 import discord
 from discord.ext import commands
 from io import BytesIO
-from network.socket_functions import SDKeyUnsealer, SocketError
+from network.socket_functions import SocketPS, SocketError
 from utils.workspace import makeWorkspace, WorkspaceError
 from utils.helpers import errorHandling
-from utils.constants import logger, Color, BASE_ERROR_MSG, IP, PORT_SDKEYUNSEALER, SEALED_KEY_ENC_SIZE
-
-class PfsSKKey:
-    def __init__(self, data: bytearray) -> None:
-        assert len(data) == self.SIZE
-
-        self.MAGIC   = data[:0x08]
-        self.VERSION = data[0x08:0x10]
-        self.IV      = data[0x10:0x20]
-        self.KEY     = data[0x20:0x40]
-        self.SHA256  = data[0x40:0x60]
-
-        self.data    = data
-
-    SIZE = 0x60
-    MAGIC_VALUE = b"pfsSKKey"
-
-    def validate(self) -> bool:
-        if self.MAGIC != self.MAGIC_VALUE:
-            return False
-        return True
-    
-    def as_bytearray(self) -> bytearray:
-        return self.data       
+from utils.constants import logger, Color, BASE_ERROR_MSG, IP, PORT_CECIE, SEALED_KEY_ENC_SIZE
+from utils.orbis import PfsSKKey
 
 class Sealed_Key(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -52,35 +30,26 @@ class Sealed_Key(commands.Cog):
             e = f"Invalid size: must be {SEALED_KEY_ENC_SIZE} bytes!"
             await errorHandling(ctx, e, workspaceFolders, None, None, None)
             return
-        
-        elif PORT_SDKEYUNSEALER is None:
-            e = "Unavailable."
-            await errorHandling(ctx, e, workspaceFolders, None, None, None)
-            return
 
         enc_key = bytearray(await sealed_key.read())
-        enc_key = PfsSKKey(enc_key)
+        sealedkey_t = PfsSKKey(enc_key)
     
-        if not enc_key.validate():
+        if not sealedkey_t.validate():
             e = "Invalid sealed key!"
             await errorHandling(ctx, e, workspaceFolders, None, None, None)
             return
 
         try:
-            unsealer = SDKeyUnsealer(IP, PORT_SDKEYUNSEALER)
-            dec_key = await unsealer.upload_key(enc_key.as_bytearray())
+            C1socket = SocketPS(IP, PORT_CECIE)
+            await C1socket.socket_decryptsdkey(sealedkey_t)
 
-            if isinstance(dec_key, str):
-                # this means error message, so raise expected error
-                raise SocketError(dec_key)
-
-            embdec= discord.Embed(title="Finished",
+            embdec = discord.Embed(title="Finished",
                           description=f"Successfully decrypted {sealed_key.filename}.",
                           colour=Color.DEFAULT.value)
             embdec.set_footer(text="Made by hzh.")
 
             await ctx.edit(embed=embdec)
-            await ctx.respond(file=discord.File(BytesIO(dec_key), filename=sealed_key.filename))
+            await ctx.respond(file=discord.File(BytesIO(sealedkey_t.dec_key), filename=sealed_key.filename))
         except SocketError as e:
             await errorHandling(ctx, e, workspaceFolders, None, None, None)
             logger.exception(f"{e} - {ctx.user.name} - (expected)")
