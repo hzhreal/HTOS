50b92074e5b6411292be20b8abf3248b69232db9
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-10-13 03:39:50 +0200

    rcube namecheck

--- data/crypto/rcube_crypt.py
+++ data/crypto/rcube_crypt.py
@@ -4,8 +4,23 @@ from data.crypto.common import CustomCrypto as CC
 
 class Crypt_RCube:
     @staticmethod
+    def namecheck(name: str | list[str]) -> bool | list[str]:
+        if isinstance(name, str):
+            return name.endswith(".dat")
+        elif isinstance(name, list):
+            valid = []
+            for path in name:
+                path_ = str(path)
+                if path_.endswith(".dat"):
+                    valid.append(path_)
+                return valid
+        else:
+            return False
+
+    @staticmethod
     async def decryptFile(folderPath: str) -> None:
         files = await CC.obtainFiles(folderPath)
+        files = await Crypt_RCube.namecheck(files)
 
         for filePath in files:
             async with aiofiles.open(filePath, "rb") as savegame:
@@ -20,6 +35,9 @@ class Crypt_RCube:
     
     @staticmethod
     async def encryptFile(fileToEncrypt: str) -> None:
+        if not Crypt_RCube.namecheck(fileToEncrypt):
+            return
+
         async with aiofiles.open(fileToEncrypt, "rb") as savegame:
             header = await savegame.read(0xC)
             decomp_data = await savegame.read()
@@ -32,6 +50,9 @@ class Crypt_RCube:
 
     @staticmethod
     async def checkEnc_ps(fileName: str) -> None:
+        if not Crypt_RCube.namecheck(fileName):
+            return
+
         null_count = 0
         async with aiofiles.open(fileName, "rb") as savegame:
             data = await savegame.read()
