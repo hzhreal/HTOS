8013ff7d3e606f4c83e114e5a908538c6df8dcc1
hzhreal <142254293+hzhreal@users.noreply.github.com>
2024-11-23 20:22:26 +0100

    JP

--- data/crypto/mgsv_crypt.py
+++ data/crypto/mgsv_crypt.py
@@ -1,32 +1,36 @@
 import hashlib
 import aiofiles
-from typing import Literal
+import struct
 from data.crypto.common import CustomCrypto as CC
 
 class Crypt_MGSV:
     MGSV_TPP_PS4KEY_CUSA01140 = 0x4131F8BE        
     MGSV_TPP_PS4KEY_CUSA01154 = 0x4F36C055
-    MGSV_TPP_PS4KEY_CUSA01099 = 0x332E72A8        
+    MGSV_TPP_PS4KEY_CUSA01099 = 0x40FDA272        
 
     MGSV_GZ_PS4KEY_CUSA00218 = 0xEA11D524        
     MGSV_GZ_PS4KEY_CUSA00211 = 0xD2225CCB        
-    MGSV_GZ_PS4KEY_CUSA00225 = 0xB2D4611B
+    MGSV_GZ_PS4KEY_CUSA00225 = 0x697B6E1B
 
     KEYS = {
         "CUSA01140": {"key": MGSV_TPP_PS4KEY_CUSA01140, "name": "MGSVTPPSaveDataNA"},
         "CUSA01154": {"key": MGSV_TPP_PS4KEY_CUSA01154, "name": "MGSVTPPSaveDataEU"},
-        "CUSA01099": {"key": MGSV_TPP_PS4KEY_CUSA01099, "name": "MGSVTPPSaveDataCK"},
+        "CUSA01099": {"key": MGSV_TPP_PS4KEY_CUSA01099, "name": "MGSVTPPSaveDataJP"},
 
         "CUSA00218": {"key": MGSV_GZ_PS4KEY_CUSA00218, "name": "MGSVGZSaveDataNA"},
         "CUSA00211": {"key": MGSV_GZ_PS4KEY_CUSA00211, "name": "MGSVGZSaveDataEU"},
-        "CUSA00225": {"key": MGSV_GZ_PS4KEY_CUSA00225, "name": "MGSVGZSaveDataCK"}
+        "CUSA00225": {"key": MGSV_GZ_PS4KEY_CUSA00225, "name": "MGSVGZSaveDataJP"}
     }
 
     HEADER_TPP = b"SV"
     HEADER_GZ = b"gz"
 
     @staticmethod
-    def crypt_data(data: list[int], length: int, title_id: Literal["CUSA01140", "CUSA01154", "CUSA01099", "CUSA00218", "CUSA00211", "CUSA00225"]) -> list[int]:
+    def get_key(archivename: str) -> int:
+        return struct.unpack("<I", hashlib.md5(bytes(archivename, "utf-8")).digest()[:4])[0]
+
+    @staticmethod
+    def crypt_data(data: list[int], length: int, title_id: str) -> list[int]:
         key = Crypt_MGSV.KEYS[title_id]["key"]
 
         for i in range(length >> 2):
@@ -38,7 +42,7 @@ class Crypt_MGSV:
         return data
 
     @staticmethod
-    async def decryptFile(folderPath: str, title_id: Literal["CUSA01140", "CUSA01154", "CUSA01099", "CUSA00218", "CUSA00211", "CUSA00225"]) -> None:
+    async def decryptFile(folderPath: str, title_id: str) -> None:
         files = await CC.obtainFiles(folderPath)
 
         for filePath in files:
@@ -55,7 +59,7 @@ class Crypt_MGSV:
                 await savegame.write(decrypted_data)
 
     @staticmethod
-    async def encryptFile(fileToEncrypt: str, title_id: Literal["CUSA01140", "CUSA01154", "CUSA01099", "CUSA00218", "CUSA00211", "CUSA00225"]) -> None:
+    async def encryptFile(fileToEncrypt: str, title_id: str) -> None:
         async with aiofiles.open(fileToEncrypt, "r+b") as savegame:
             decrypted_data = bytearray(await savegame.read())
 
@@ -74,7 +78,7 @@ class Crypt_MGSV:
             await savegame.write(encrypted_data)
 
     @staticmethod
-    async def checkEnc_ps(fileName: str, title_id: Literal["CUSA01140", "CUSA01154", "CUSA01099", "CUSA00218", "CUSA00211", "CUSA00225"]) -> None:
+    async def checkEnc_ps(fileName: str, title_id: str) -> None:
         async with aiofiles.open(fileName, "rb") as savegame:
             await savegame.seek(0x10)
             header = await savegame.read(2)
@@ -83,7 +87,7 @@ class Crypt_MGSV:
             await Crypt_MGSV.encryptFile(fileName, title_id)
 
     @staticmethod
-    async def reregion_changeCrypt(folderPath: str, target_titleid: Literal["CUSA01140", "CUSA01154", "CUSA01099", "CUSA00218", "CUSA00211", "CUSA00225"]) -> None:
+    async def reregion_changeCrypt(folderPath: str, target_titleid: str) -> None:
         files = await CC.obtainFiles(folderPath)
 
         for filePath in files:
