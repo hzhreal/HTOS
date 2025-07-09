b160d90119c4bc9d25bada1cf2d389760c9131fd
hzhreal <142254293+hzhreal@users.noreply.github.com>
2025-07-09 14:03:08 +0200

    alt semaphore

--- network/socket_functions.py
+++ network/socket_functions.py
@@ -18,11 +18,12 @@ class SocketPS:
     self.host = host
     self.port = port
     self.semaphore = asyncio.Semaphore(maxConnections) # Maximum 16 mounts at once
+    self.semaphore_alt = asyncio.Semaphore(maxConnections) # For operations that does not need a mount slot
   SUCCESS = "srOk"
-  async def send_tcp_message_with_response(self, message: str, deserialize: bool = True) -> str | bytes:
+  async def send_tcp_message_with_response(self, message: str, semaphore: asyncio.Semaphore, deserialize: bool = True) -> str | bytes:
     writer = None
     try:
-      async with self.semaphore:
+      async with semaphore:
         reader, writer = await asyncio.open_connection(self.host, self.port)
         writer.write(message.encode("utf-8"))
         await writer.drain()
@@ -44,9 +45,14 @@ class SocketPS:
         await writer.wait_closed()
       
   async def testConnection(self) -> None:
-    _, writer = await asyncio.wait_for(asyncio.open_connection(self.host, self.port), timeout=10)
-    writer.close()
-    await writer.wait_closed()
+    writer = None
+    try:
+      async with self.semaphore_alt:
+        _, writer = await asyncio.wait_for(asyncio.open_connection(self.host, self.port), timeout=10)
+    finally:
+      if writer is not None:
+        writer.close()
+        await writer.wait_closed()
 
   async def socket_dump(self, folder: str, savename: str) -> None: 
     request = json.dumps({
@@ -58,7 +64,7 @@ class SocketPS:
       }
     }) + "\r\n"
 
-    response = await self.send_tcp_message_with_response(request)
+    response = await self.send_tcp_message_with_response(request, self.semaphore)
     
     if response.get("ResponseType") != self.SUCCESS:
       raise SocketError(response.get("code", "Failed to dump save!"))
@@ -73,7 +79,7 @@ class SocketPS:
       }
     }) + "\r\n"
 
-    response = await self.send_tcp_message_with_response(message)
+    response = await self.send_tcp_message_with_response(message, self.semaphore)
 
     if response.get("ResponseType") != self.SUCCESS:
       raise SocketError(response.get("code", "Failed to update save!"))
@@ -83,7 +89,7 @@ class SocketPS:
       "RequestType": "rtKeySet"
     }) + "\r\n"
 
-    response = await self.send_tcp_message_with_response(message)
+    response = await self.send_tcp_message_with_response(message, self.semaphore_alt)
 
     return response.get("keyset", "FAIL!")
   
@@ -97,7 +103,7 @@ class SocketPS:
       }
     }) + "\r\n"
 
-    response = await self.send_tcp_message_with_response(message)
+    response = await self.send_tcp_message_with_response(message, self.semaphore)
 
     if response.get("ResponseType") != self.SUCCESS:
       raise SocketError(response.get("code", "Failed to create a save!"))
@@ -110,7 +116,7 @@ class SocketPS:
       }
     }) + "\r\n"
 
-    response = await self.send_tcp_message_with_response(message)
+    response = await self.send_tcp_message_with_response(message, self.semaphore_alt)
 
     if response.get("ResponseType") == "srInvalid":
       raise SocketError("Failed to decrypt sealed key!")
