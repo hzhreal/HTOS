c362f2ad75ee876860f41ad33895fae6d2aa0e6f
hzhreal <142254293+hzhreal@users.noreply.github.com>
2026-02-19 23:45:03 +0100

    implement instancelock timeout (#21)
    * implement instancelock timeout

--- utils/instance_lock.py
+++ utils/instance_lock.py
@@ -1,41 +1,75 @@
 import asyncio
+import time
 from dataclasses import dataclass, field
+from utils.conversions import hours_to_seconds
 from utils.exceptions import InstanceError
 
+INSTANCE_TIMEOUT = hours_to_seconds(1) # a single instance will be lazily freed up every hour for each user if possible
 MAXIMUM_INSTANCES_AT_ONCE = 32
 MAXIMUM_INSTANCES_PER_USER = 1
+
+@dataclass
+class Instance:
+    active_instances: int = 0
+    timestamp: float = field(default_factory=time.time)
 
 @dataclass
 class InstanceLock:
     maximum_instances: int
     maximum_instances_per_user: int
 
-    instances: dict[int, int] = field(default_factory=dict)
+    instances: dict[int, Instance] = field(default_factory=dict)
     instances_len: int = 0
 
     def __post_init__(self) -> None:
         self.lock = asyncio.Lock()
 
+    def _timeout_handler(self, disc_userid: int) -> None:
+        """Call inside lock."""
+
+        if not disc_userid in self.instances:
+            return
+
+        epoch = time.time()
+        instance = self.instances[disc_userid]
+
+        # check if timeout has passed
+        if epoch - instance.timestamp >= INSTANCE_TIMEOUT:
+            # free a slot, no need to delete it (if active_instances == 0) nor set new timestamp because it will be used in acquire
+            instance.active_instances -= 1
+            self.instances_len -= 1
+
     async def acquire(self, disc_userid: int) -> None:
         async with self.lock:
+            self._timeout_handler(disc_userid)
+
             if self.instances_len == self.maximum_instances:
                 raise InstanceError(f"There are no available slots! Maximum of {self.maximum_instances} reached. Please try again later.")
 
-            user_occupied_slots = self.instances.get(disc_userid, 0)
-            if user_occupied_slots == self.maximum_instances_per_user:
+            instance = self.instances.get(disc_userid)
+            if not instance:
+                instance = Instance()
+                self.instances[disc_userid] = instance
+
+            if instance.active_instances == self.maximum_instances_per_user:
                 raise InstanceError(f"You can only have {self.maximum_instances_per_user} active instance(s) at a time!")
 
-            self.instances[disc_userid] = user_occupied_slots + 1
+            # every time a new slot is given, recalculate the timestamp
+            instance.timestamp = time.time()
+
+            instance.active_instances += 1
             self.instances_len += 1
 
     async def release(self, disc_userid: int) -> None:
         async with self.lock:
-            assert disc_userid in self.instances and self.instances_len > 0
+            if not disc_userid in self.instances or self.instances_len == 0:
+                return
 
-            self.instances[disc_userid] -= 1
+            instance = self.instances[disc_userid]
+            instance.active_instances -= 1
             self.instances_len -= 1
 
-            if self.instances[disc_userid] == 0:
+            if instance.active_instances == 0:
                 del self.instances[disc_userid]
 
 INSTANCE_LOCK_global = InstanceLock(MAXIMUM_INSTANCES_AT_ONCE, MAXIMUM_INSTANCES_PER_USER)
