a17b6b25e56de9485d34d4e24d7ec118c7109e72
hzhreal <142254293+hzhreal@users.noreply.github.com>
2026-06-05 16:46:41 +0200

    doa5-lr (#54)

--- data/crypto/helpers.py
+++ data/crypto/helpers.py
@@ -18,7 +18,8 @@ from utils.constants import (
     DIGIMON_TITLEID, SDEW_TITLEID, NIOH2_TITLEID, MHWI_TITLEID, RE_VILLAGE_TITLEID, LA_NOIRE_TITLEID, LOH_TRAILS_CS4_TITLEID,
     LOH_TRAILS_DAYBREAK_TITLEID, LOH_TRAILS_ZERO_AZURE, MINECRAFT_TITLEID, FF7CC_TITLEID, TOSR_TITLEID, RE5_TITLEID, CCR_TITLEID,
     TOB_TITLEID, TR6R_TITLEID, STRIDER_TITLEID, DIABLO3_TITLEID, ALIEN_ISO_TITLEID, SHANTAE_SCURSE_TITLEID, MAFIA3_TITLEID, DEADRISING_TITLEID,
-    KH3_TITLEID, PO_PERSIA_TITLEID, LUNAR_R_TITLEID, DSTRANDING_TITLEID, FC5_TITLEID, FF_PIXEL_TITLEID, SAO_FB_TITLEID, MHR_TITLEID
+    KH3_TITLEID, PO_PERSIA_TITLEID, LUNAR_R_TITLEID, DSTRANDING_TITLEID, FC5_TITLEID, FF_PIXEL_TITLEID, SAO_FB_TITLEID, MHR_TITLEID,
+    DOA5_LR_TITLEID
 )
 from utils.embeds import embdecTimeout, embdecFormat, embErrdec
 from utils.extras import generate_random_string
@@ -132,6 +133,8 @@ async def extra_decrypt(
                         await Crypto.PoPersia.check_dec_ps(folderpath)
                     case "FFPIXEL":
                         await Crypto.FFPixel.check_dec_ps(folderpath)
+                    case "DOA5LR":
+                        await Crypto.DoA5LR.check_dec_ps(folderpath)
             except (ValueError, IOError, IndexError):
                 raise CryptoError("Invalid save!")
 
@@ -562,6 +565,18 @@ async def extra_decrypt(
         await d_ctx.msg.edit(embed=emb, view=CryptChoiceButton("FFPIXEL"))
         await helper.await_done()
 
+    elif title_id in DOA5_LR_TITLEID:
+        if choice is not None:
+            if choice:
+                try:
+                    await Crypto.DoA5LR.check_dec_ps(folderpath)
+                except (ValueError, IOError, IndexError):
+                    raise CryptoError("Invalid save!")
+            return
+
+        await d_ctx.msg.edit(embed=emb, view=CryptChoiceButton("DOA5LR"))
+        await helper.await_done()
+
 async def extra_import(title_id: str, folderpath: str, savepairname: str) -> None:
     try:
         if title_id in GTAV_TITLEID:
@@ -710,6 +725,9 @@ async def extra_import(title_id: str, folderpath: str, savepairname: str) -> Non
 
         elif title_id in MHR_TITLEID:
             await Crypto.MHR.check_enc_ps(folderpath)
+
+        elif title_id in DOA5_LR_TITLEID:
+            await Crypto.DoA5LR.check_enc_ps(folderpath)
     except (ValueError, IOError, IndexError):
         raise CryptoError("Invalid save!")
 
