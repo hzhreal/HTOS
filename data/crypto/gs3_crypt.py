4f8d59c1ed4b09fedf8bbf15907c3b0f5f820722
hzhreal <142254293+hzhreal@users.noreply.github.com>
2026-06-23 00:38:59 +0200

    keep header

--- data/crypto/gs3_crypt.py
+++ data/crypto/gs3_crypt.py
@@ -1,3 +1,5 @@
+from poplib import CR
+
 import aiofiles
 from data.crypto.common import CustomCrypto as CC
 from data.crypto.exceptions import CryptoError
@@ -13,11 +15,13 @@ class Crypt_GS3:
     MAGIC = bytes([
         0x1A, 0x47, 0x77, 0x92, 0x4F, 0x98, 0xF2, 0x82
     ])
+    DEC_MAGIC = b"GVAS"
+    HEADER_SIZE = 16
 
     @staticmethod
     async def decrypt_file(filepath: str) -> None:
         async with CC(filepath) as cc:
-            buf = await cc.ext_read(8, 8)
+            buf = await cc.ext_read(Crypt_GS3.HEADER_SIZE - 8, 8)
             decomp_size = uint32(buf[:4], "little").value
             if decomp_size > cc.SAVESIZE_MAX:
                 raise CryptoError("Unsupported save!")
@@ -26,16 +30,18 @@ class Crypt_GS3:
                 raise CryptoError("Invalid save!")
 
             aes = cc.create_ctx_aes(Crypt_GS3.SECRET_KEY, cc.AES.MODE_ECB)
-            cc.set_ptr(16)
+            cc.set_ptr(Crypt_GS3.HEADER_SIZE)
             while await cc.read():
                 cc.decrypt(aes)
                 await cc.write()
         async with CC(filepath, in_place=False) as cc:
-            await cc.copy(16, 16 + cipher_size)
+            await cc.copy(0, Crypt_GS3.HEADER_SIZE + cipher_size)
             await cc.pkcs7_pad_post(cc.AES.block_size)
         async with CC(filepath, in_place=False) as cc:
             zlib = cc.create_ctx_zlib_decompress()
             l = 0
+            await cc.copy(0, Crypt_GS3.HEADER_SIZE)
+            cc.set_ptr(Crypt_GS3.HEADER_SIZE)
             while await cc.read():
                 l += await cc.decompress(zlib)
                 if l > decomp_size:
@@ -46,11 +52,13 @@ class Crypt_GS3:
     @staticmethod
     async def encrypt_file(filepath: str) -> None:
         async with CC(filepath, in_place=False) as cc:
-            decomp_size = uint32(cc.size, "little").as_bytes
-            header = Crypt_GS3.MAGIC + decomp_size + bytes(4)
-            await cc.w_stream.write(header)
+            await cc.copy(0, Crypt_GS3.HEADER_SIZE)
+            decomp_size = uint32(cc.size - Crypt_GS3.HEADER_SIZE, "little").as_bytes
+            await cc.w_stream.seek(Crypt_GS3.HEADER_SIZE - 8)
+            await cc.w_stream.write(decomp_size)
             zlib = cc.create_ctx_zlib_compress()
             l = 0
+            cc.set_ptr(Crypt_GS3.HEADER_SIZE)
             while await cc.read():
                 l += await cc.compress(zlib)
             l += await cc.compress_post(zlib)
@@ -58,12 +66,12 @@ class Crypt_GS3:
             l_ = uint32(l, "little")
             if l != l_.value:
                 raise CryptoError("Invalid save!")
-            await cc.w_stream.seek(12)
+            await cc.w_stream.seek(Crypt_GS3.HEADER_SIZE - 4)
             await cc.w_stream.write(l_.as_bytes)
             await cc.pkcs7_pad_pre(cc.AES.block_size)
         async with CC(filepath) as cc:
             aes = cc.create_ctx_aes(Crypt_GS3.SECRET_KEY, cc.AES.MODE_ECB)
-            cc.set_ptr(16)
+            cc.set_ptr(Crypt_GS3.HEADER_SIZE)
             while await cc.read():
                 cc.encrypt(aes)
                 await cc.write()
@@ -73,8 +81,10 @@ class Crypt_GS3:
         files = await CC.obtain_files(folderpath)
         for filepath in files:
             async with aiofiles.open(filepath, "rb") as savegame:
-                m = await savegame.read(len(Crypt_GS3.MAGIC))
-            if m == Crypt_GS3.MAGIC:
+                m1 = await savegame.read(len(Crypt_GS3.MAGIC))
+                await savegame.seek(Crypt_GS3.HEADER_SIZE)
+                m2 = await savegame.read(len(Crypt_GS3.DEC_MAGIC))
+            if m1 == Crypt_GS3.MAGIC and m2 != Crypt_GS3.DEC_MAGIC:
                 await Crypt_GS3.decrypt_file(filepath)
 
     @staticmethod
@@ -82,7 +92,9 @@ class Crypt_GS3:
         files = await CC.obtain_files(folderpath)
         for filepath in files:
             async with aiofiles.open(filepath, "rb") as savegame:
-                m = await savegame.read(len(Crypt_GS3.MAGIC))
-            if m != Crypt_GS3.MAGIC:
+                m1 = await savegame.read(len(Crypt_GS3.MAGIC))
+                await savegame.seek(Crypt_GS3.HEADER_SIZE)
+                m2 = await savegame.read(len(Crypt_GS3.DEC_MAGIC))
+            if m1 == Crypt_GS3.MAGIC and m2 == Crypt_GS3.DEC_MAGIC:
                 await Crypt_GS3.encrypt_file(filepath)
 
