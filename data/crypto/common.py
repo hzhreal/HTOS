f87fccf78cd53101e6c120da6f704a40f5952fdb
hzhreal <142254293+hzhreal@users.noreply.github.com>
2026-07-17 17:46:06 +0200

    fix: global SAVESIZE_MAX was being used in some places

--- data/crypto/common.py
+++ data/crypto/common.py
@@ -20,7 +20,7 @@ from aiofiles.threadpool.binary import AsyncBufferedReader
 from Crypto.Cipher import AES, Blowfish, DES3
 from typing import Literal, Any, Self
 
-from utils.constants import SCE_SYS_NAME, RANDOMSTRING_LENGTH, SAVESIZE_MAX
+from utils.constants import SCE_SYS_NAME, RANDOMSTRING_LENGTH
 # from utils.constants import SAVESIZE_MAX
 from utils.conversions import mb_to_bytes, bytes_to_mb
 from utils.type_helpers import Cint, uint8, uint32, uint64
@@ -686,7 +686,7 @@ class CustomCrypto:
 
     async def _comp_write(self, w: bytes, size: int) -> int:
         l = len(w)
-        if size + l > SAVESIZE_MAX:
+        if size + l > self.SAVESIZE_MAX:
             raise CryptoError("Max size reached while compressing!")
         await self.w_stream.write(w)
         return l
@@ -711,7 +711,7 @@ class CustomCrypto:
 
     async def _decomp_write(self, w: bytes, size: int) -> int:
         l = len(w)
-        if size + l > SAVESIZE_MAX:
+        if size + l > self.SAVESIZE_MAX:
             raise CryptoError("Max size reached while decompressing!")
         await self.w_stream.write(w)
         return l
